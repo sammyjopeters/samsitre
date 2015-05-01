e568087c76c09707db505d977c7bc650da63cf2c
sammyjopeters <peters.sammyjo@gmail.com>
2015-05-01 18:42:06 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Samsitre::Application.config.secret_key_base = '6d9a930229eea4f961c72a6a81ce3358879d2bf1fe2b51037b14784f42a15cb10f10e9322973c6ddc94e85b604a3c890dbf4a85b849f3c71a0d7557d3517a7bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Samsitre::Application.config.session_store :cookie_store, key: '_samsitre_session'
